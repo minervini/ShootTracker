c8b03dac9d2a66e60f626e205cca9585c6eaf807
Stefano D Minervini <minervini@mac.com>
2019-10-30 12:29:51 -0500

    added properties page

---
+++ ShootTracker/Models/TestShoot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ShootTracker.Models
+{
+    public class TestShoot
+    {
+        public int ID { get; set; }
+        public string Good { get; set; }
+        public string ShotName { get; set; }
+        public int Take { get; set; }
+        public DateTime In_TC { get; set; }
+        public DateTime Out_TC { get; set; }
+        public DateTime Duration { get; set; }
+        public string AudioChannel_1 { get; set; }
+        public string AudioChannel_2 { get; set; }
+        public string Commits { get; set; }
+    }
+}
